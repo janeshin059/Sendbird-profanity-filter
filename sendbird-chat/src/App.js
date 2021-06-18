b6f7adfa3769ea71f9d49d2f95b8fee2df5afa9b
janeshin059 <janeshin059@gmail.com>
2021-06-18 15:01:43 +0900

    Client Firebase integration

--- sendbird-chat/src/App.js
+++ sendbird-chat/src/App.js
@@ -3,8 +3,20 @@ import './App.css';
 import { App as SendbirdApp } from "sendbird-uikit";
 import "sendbird-uikit/dist/index.css";
 import {APP_ID, USER_ID} from './const.js'
+import firebase from './firebase'
+import React from 'react';
 
 function App() {
+  React.useEffect(() => {
+  const msg = firebase.messaging();
+  msg.requestPermission().then(() => {
+    return msg.getToken();
+
+  }).then((data)=>{
+    console.warn("token",data)
+  })
+  }
+  )
   return (
     <div className="App" style={{height:600}}>
       <SendbirdApp appId={APP_ID} userId={USER_ID}></SendbirdApp>
