1c708fdf63fed1c42ac6363fc97246e37047adcb
janeshin059 <janeshin059@gmail.com>
2021-06-19 21:54:03 +0900

    Changed /express_backend to /hook

--- sendbird-chat/src/App.js
+++ sendbird-chat/src/App.js
@@ -1,36 +1,35 @@
-import './App.css';
+import "./App.css";
 import { App as SendbirdApp } from "sendbird-uikit";
 import "sendbird-uikit/dist/index.css";
-import {APP_ID, NICKNAME, USER_ID} from './const.js'
-import React from 'react';
-import { requestFirebaseNotificationPermission } from './firebaseInit'
+import { APP_ID, NICKNAME, USER_ID } from "./const.js";
+import React from "react";
+import { requestFirebaseNotificationPermission } from "./firebaseInit";
 
 function App() {
-  
-  // fetching the GET route from the Express server which matches the GET route from server.js
- const callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
 
+  const callBackendAPI = async () => {
+    const response = await fetch('/hook');
+    const body = await response.json();
     if (response.status !== 200) {
-      throw Error(body.message) 
+      throw Error(body.message);
     }
     return body;
   };
 
-  //componentDidMount() {
-    callBackendAPI()
-      .catch(err => console.log(err));
-  //};
+  callBackendAPI().catch((err) => console.log(err));
 
-  //Request the browser's permission to send notifications 
-  if(!localStorage.getItem("notification-token")){
-  requestFirebaseNotificationPermission();
+  //Request the browser's permission to send notifications
+  if (!localStorage.getItem('notification-token')) {
+    requestFirebaseNotificationPermission();
   }
 
   return (
-    <div className="App" style={{height:600}}>
-      <SendbirdApp appId={APP_ID} userId={USER_ID} nickname={NICKNAME}></SendbirdApp>
+    <div className="App" style={{ height: 600 }}>
+      <SendbirdApp
+        appId={APP_ID}
+        userId={USER_ID}
+        nickname={NICKNAME}
+      ></SendbirdApp>
     </div>
   );
 }
--- sendbird-chat/src/server/server.js
+++ sendbird-chat/src/server/server.js
@@ -3,7 +3,6 @@ var firebase = require("firebase");
 
 const express = require("express");
 var admin = require("firebase-admin");
-const crypto = require('crypto');
 
 //Firebase messaging setup
 const serviceAccount = require('./sendbird-app-firebase-admin.json');
@@ -31,11 +30,10 @@ app.use(express.urlencoded({ extended: true }));
 let tokens = ['cd0dfjDqetvMf_-RKDD9IT:APA91bHoxF51nXzNmaIizvRhEXj_0m0pfnuS9VJRP-9f8FOx3qcVrraY1g4G2rvDk7Y6QJkyQb160V_cdx2s-xetM9i-AdRGl9Qz_y2QjZ_4Hlw7eom79O21pdNnAZY6f_w26JFvMR2t'];
 
 //TODO:Sendbird Webhook
-app.post('/express_backend', (req, res) => {
+app.post('/hook', (req, res) => {
   const body = req.body;
 
   console.log(body);
-
   if (body.category === "profanity_filter:replace") {
     console.log('profanity replaced')
     notify(body.payload.message);
@@ -64,7 +62,6 @@ function notify(body) {
 
 app.listen(PORT, () => console.log(`App running at port ${PORT}!`));
 
-
-app.get('/express_backend', (req, res) => { 
+app.get('/hook', (req, res) => { 
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
 }); 
